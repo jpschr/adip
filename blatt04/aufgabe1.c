2480edd1542df1ff63e855b280e4a34ac6d9f890
jpschr <jpschr@uni-bonn.de>
2017-11-17 01:02:29 +0100

    Revert last commit, restore old version of aufgabe1.c blatt04

--- blatt04/aufgabe1.c
+++ blatt04/aufgabe1.c
@@ -1,46 +1,88 @@
 #include <stdio.h>
-unsigned long long fac(int n, int k){
-	int i = 0;
-	unsigned long long fac = 1;
-	
-	if(n == 0){
-		return 1;
-	}
-	for(i = n; i > k; i--){
-		fac *= i;
+#include <stdlib.h>
+
+int fac(int n){
+	int i;
+	int fac=1;
+	if(n<0) return -1;
+	if(n==0) return 1;
+	for(i=1;i<=n;i++){
+		fac*=i;
 	}
 	return fac;
 }
 
-unsigned long long bincoef(int n, int k){
+int n_over_k(int n, int k){
+	if(n<0||k<0) return -1;
+	if(n<k) return 0;
+	return fac(n)/(fac(n-k)*fac(k));
+}
 
-	if(n < k){
+int test_fac(){
+	int n;
+	int nfac;
+	printf("Bitte eine nichtnegative ganze Zahl n eingeben:");
+	scanf("%i", &n);
+	nfac=fac(n);
+	if(nfac==-1){
+		printf("Die Zahl muss nichtnegativ sein!\n");
 		return 0;
 	}
-	return (fac(n,k)/(fac(n-k, 0)));
-}
-
-unsigned long long lotto(int n, int k){
-	return (bincoef(n, k) * fac(k, 0));
-}
-
-
-int main(){
-	int n = -1,k = -1;
-	
-	
-	while(n < 0){
-		printf("Bitte geben sie eine natürliche Zahl ein: \n");
-		scanf("%i" , &n);
-	}
-	
-	while((k < 0)){
-		printf("Bitte geben sie eine natürliche Zahl ein: \n");
-		scanf("%i" , &k);
-	}
-	
-	printf("Der Binomialkoeffizient ist: %llu\n", bincoef(n,k));
-	printf("modifiziertes Lottoproblem : %llu\n", lotto(n, k));
-	
+	printf("%i!=%i\n",n, nfac);
 	return 0;
 }
+
+int test_n_over_k(){
+	int n,k;
+	int nok;
+	printf("Bitte n>=0 eingeben:");
+	scanf("%i", &n);
+	printf("Bitte k>=0 eingeben:");
+	scanf("%i", &k);
+	nok=n_over_k(n,k);
+	if(nok==-1){
+		printf("Eingabe unzulässig!\n");
+		return EXIT_FAILURE;
+	}
+	printf("n über k ist %i\n", nok);
+	return EXIT_SUCCESS;
+}
+
+int lotto(int n, int k){
+	int nok;
+	nok=n_over_k(n,k);
+	if(nok==-1){ /*n_over_k checks if n and k are not-negative*/
+		return -1;
+	}	
+	return n_over_k(n,k)*fac(k);
+
+}
+
+int test_lotto(){
+	int n,k;
+	int lot;
+	printf("Bitte n>=0 eingeben:");
+	scanf("%i", &n);
+	printf("Bitte k>=0 eingeben:");
+	scanf("%i", &k);
+	/*if(n<0||k<0){
+		printf("Eingabe unzulässig!");
+		return EXIT_FAILURE;
+	}*/
+	lot=lotto(n,k);
+	if(lot==-1){
+		printf("Fehler!");
+		return EXIT_FAILURE;
+	}
+	printf("lotto= %i\n", lot);
+	return EXIT_SUCCESS;
+
+}
+
+int main(){
+	test_fac();
+	test_n_over_k();
+	test_lotto();
+	return EXIT_SUCCESS;
+}
+
