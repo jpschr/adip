7a5f4ec756df9453c5e2817bb49743efe2ac065a
jpschr <jpschr@uni-bonn.de>
2017-11-23 12:43:12 +0100

    Modify aufgabe2.c blatt05

--- blatt05/aufgabe2.c
+++ blatt05/aufgabe2.c
@@ -14,7 +14,7 @@ int* array;
 int size;
 };
 
-void dyn_array_add(struct DynArray* dynarray, int new_elem){
+int dyn_array_add(struct DynArray* dynarray, int new_elem){
 	int* n_array=NULL;
 	if(dynarray->elements<dynarray->max_size){
 		dynarray->array[dynarray->elements]=new_elem;
@@ -23,31 +23,32 @@ void dyn_array_add(struct DynArray* dynarray, int new_elem){
 	else{						
 		n_array=malloc(2*dynarray->max_size*sizeof(int));
 		if(NULL==n_array){
-			printf("Speicherallokieren fehlgeschlagen");
+			return 0;
 		}else{
 
 			memcpy(n_array,dynarray->array, dynarray->max_size*sizeof(int));
 			free(dynarray->array);
 			dynarray->array=n_array;
 			dynarray->max_size*=2;
-			/* free(dynarray->array); */ /* TODO: When done this way we get a use-after-free, need other way to free this*/
 			dyn_array_add(dynarray, new_elem);
 		}
 	}
+	return 1;
 }
 
-void dyn_array_min_add(struct DynArrayMin* dynarraymin, int new_elem){
+int dyn_array_min_add(struct DynArrayMin* dynarraymin, int new_elem){
 	int* n_array=NULL;
 	n_array=malloc((dynarraymin->size+1)*sizeof(int));
 	if(NULL==n_array){
-		printf("Speicherallkoieren fehlgeschlagen");
+		return 0;
 	}else{
 		memcpy(n_array,dynarraymin->array, dynarraymin->size*sizeof(int));
-		/* free(dynarraymin->array); */
+		free(dynarraymin->array);
 		dynarraymin->array=n_array;
 		dynarraymin->array[dynarraymin->size]=new_elem;
 		dynarraymin->size++;
 	}
+	return 1;
 }
 
 void dyn_array_print(struct DynArray* dynarray){
@@ -102,16 +103,19 @@ int main(){
 	dynarray.array=malloc(dynarray.max_size*sizeof(int));
 	if(NULL==dynarray.array) return EXIT_FAILURE;
 	/* initialize dynarraymin */
-	dynarraymin.size=1;
+	/*dynarraymin.size=1;
 	dynarraymin.array=malloc(dynarraymin.size*sizeof(int));
 	if(NULL==dynarraymin.array) return EXIT_FAILURE;
-	dynarray.array[0]=rand()%100; /* TODO: This line has no effect to output*/
+	dynarraymin.array[0]=rand()%100;*/
+	dynarraymin.size=0;
+	dynarraymin.array=NULL;
 	printf("Test von DynArray\n");
 	for(i=0;i<test_size;i++){
 		dyn_array_add(&dynarray,rand()%100);
 		dyn_array_print_full(&dynarray);
 	}
 	printf("Test von DynArrayMin\n");
+	dyn_array_min_print(&dynarraymin);
 	for(i=0;i<test_size;i++){
 		dyn_array_min_add(&dynarraymin, rand()%100);
 		dyn_array_min_print(&dynarraymin);
